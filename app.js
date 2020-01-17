3794f5fdd1f2e30ce2626ddd6bb4b1e7378f4b95
Tom Hewitt <tommhewitt@gmail.com>
2020-01-17 11:31:04 +0000

    💻 Add app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const listEndpoints = require('express-list-endpoints');
+const Splitwise = require('splitwise');
+const { version } = require('./package.json');
+require('dotenv').config();
+
+const app = express();
+const groupRoutes = require('./routes/group-routes');
+
+const successCode = 200;
+const clientErrorCode = 400;
+const errorCode = 502;
+
+const groupId = process.env.GROUP_ID;
+
+const sw = Splitwise({
+    consumerKey: process.env.CONSUMER_KEY,
+    consumerSecret: process.env.CONSUMER_SECRET,
+});
+
+// sw.getGroup({ id: groupId }).then(console.log);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/group', groupRoutes);
+
+app.get('/', (req, res) => {
+    res.status(successCode).send({ status: '💰 SERVER OK', version, endpoints: listEndpoints(app) });
+});
+
+const port = 3001;
+app.listen(process.env.PORT || port, () => { });
+
+module.exports = app;
